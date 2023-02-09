ab834fb63bacf6a38b12d19be7b388013bbc1780
koecingkita <ibnuyusuf1243@gmail.com>
2023-02-09 21:37:06 +0700

    first commit node

---
+++ 3-modules.js
@@ -0,0 +1,11 @@
+// Modules, every file is module (by default)
+// Modules - Encapsulated Code (only share minimun)
+const names = require('./4-name');
+const functionnyaBisaBerubahBroHAHA = require('./5-utils');
+const data = require('./6-alternative-flavor');
+require('./7-mind-grenade')
+console.log(data);
+
+functionnyaBisaBerubahBroHAHA('susan');
+functionnyaBisaBerubahBroHAHA(names.john);
+functionnyaBisaBerubahBroHAHA(names.peter);
